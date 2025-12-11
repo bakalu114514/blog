f8f5ab791e9be047100a589cd349d230ed49d615
bakalu <bakalu@example.com>
2025-12-11 15:09:03 +0800

    更改1

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,27 @@
+import * as clientConfig0 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-hea_f6d78ba60b44e983b4dcdc2f6a907e9f/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-to_11e19376b9f7d730c6be098cc7471d8d/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@_d5e54ea5f4798014fa706a810c5b2221/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-h_70fecde9aed6d175fdc2982ee6a6c54c/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/docs/.vuepress/.temp/git/config.js'
+import * as clientConfig5 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoo_1c26514bdc54d70a55d2d8f15c9f9e21/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig6 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@_de33f7e6c42e6a1f38eefae36962e0a4/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig7 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig8 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig9 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data_a8987116a441d4b13394ba96a52f4dc1/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig10 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0_71578d49751baa8b47d991b46f439dd2/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig11 from 'D:/桌面/web study test/vuepress_blog/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+  clientConfig11,
+].map((m) => m.default).filter(Boolean)
